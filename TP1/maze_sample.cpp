809a3f7f1bfe208692390fa6219e7a2f283ca71b
andreloppes <andre_loppes@outlook.com>
2023-03-17 17:16:14 -0300

    Update maze_sample.cpp

--- TP1/maze_sample.cpp
+++ TP1/maze_sample.cpp
@@ -23,9 +23,10 @@ std::stack<pos_t> valid_positions;
 // Inserir elemento:
 
 pos_t pos;
-pos.i = 1;
+
+/*pos.i = 1;
 pos.j = 3;
-valid_positions.push(pos);
+valid_positions.push(pos);*/
 
 // Retornar o numero de elementos: valid_positions.size();
 // Retornar o primeiro elemento fo vetor: valid_positions.pop();
@@ -65,7 +66,7 @@ pos_t load_maze(const char *file_name) {
       // Le o valor da linha i+1,j do arquivo e salva na posição maze[i][j]
       fscanf(pFile, "%c", &n);
       maze[i + 1][j] = n;
-      // Se o valor for 'e' salvar o valor em initial_pos
+      // Se o valor for 'i' salvar o valor em initial_pos
       if (n == 'i') {
         initial_pos.i = i;
         initial_pos.j = j;
@@ -84,7 +85,7 @@ void print_maze() {
   }
 }
 
-void walk(post_t pos) {
+void walk(pos_t pos) {
   // Marcar a posição atual com o símbolo '.'
   maze[pos.i][pos.j] = '.';
   /* Dado a posição atual, verifica quais sao as próximas posições válidas
@@ -99,18 +100,63 @@ e se são posições ainda não visitadas (ou seja, caracter 'x') e
            Caso alguma das posiçÕes validas seja igual a 's', terminar o
      programa
    */
+  if(pos.i > 0 && pos.i < num_rows && pos.j > 0 && pos.j < num_cols){
+    // LEFT
+    if(maze[pos.i][pos.j+1] == 'x'){
+      pos_t valid_pos;
+      valid_pos.i = pos.i;
+      valid_pos.j = pos.j+1;
+      valid_positions.push(valid_pos);
+    }
+    if(maze[pos.i][pos.j+1] == 's'){
+      return;
+    }
+
+    // RIGHT
+    if(maze[pos.i][pos.j-1]){
+    pos_t valid_pos;
+      valid_pos.i = pos.i;
+      valid_pos.j = pos.j-1;
+      valid_positions.push(valid_pos);
+    }
+    if(maze[pos.i][pos.j-1] == 's'){
+      return;
+    }
+
+    // UP
+    if(maze[pos.i+1][pos.j]){
+    pos_t valid_pos;
+      valid_pos.i = pos.i+1;
+      valid_pos.j = pos.j;
+      valid_positions.push(valid_pos);
+    }
+    if(maze[pos.i+1][pos.j] == 's'){
+      return;
+    }
+
+    // DOWN
+    if(maze[pos.i-1][pos.j]){
+    pos_t valid_pos;
+      valid_pos.i = pos.i-1;
+      valid_pos.j = pos.j;
+      valid_positions.push(valid_pos);
+    }
+    if(maze[pos.i-1][pos.j] == 's'){
+      return;
+    }
+  }
 
   // Imprime o labirinto
-  print_maze()
+  print_maze();
       // Verifica se o vetor nao esta vazio.
       // Caso não esteja, pegar o primeiro valor de
       // valid_positions, remove-lo e chamar a funçao walk com esse valor
-      pos_t next_position = valid_positions.front();
+      pos_t next_position = valid_positions.top();
   valid_positions.pop();
 }
 
 int main(int argc, char *argv[]) {
   // carregar o labirinto com o nome do arquivo recebido como argumento (argv[])
-  pos_t initial_pos = load_maze(maze.txt);
+  pos_t initial_pos = load_maze("maze.txt");
   walk(initial_pos);
 }
